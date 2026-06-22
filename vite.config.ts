39c06ead9c95e86ac8532549c22007fdc34081c4
Nicolas Chamorro <nichagiro@gmail.com>
2026-06-22 15:35:18 -0500

    fix: externalize jsx-runtime and fix types path in package.json

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
       fileName: 'ui-primitives',
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ['react', 'react-dom', 'react/jsx-runtime', 'react/jsx-dev-runtime'],
       output: {
         globals: {
           react: 'React',
